192c25e637eda734db1ff5a1be02e5cb1c8215e8
Whitedevil <105511358+whitedevil222@users.noreply.github.com>
2022-12-31 18:34:10 +0530

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,34 @@
+from turtle import *
+
+bgcolor("black")
+t = [Turtle(), Turtle()]
+x = 6
+colors = ["red", "yellow", "blue", "lime"]
+for index, i in enumerate(t):
+    i.speed("fastest")
+    i.color("white")
+    i.shape("circle")
+    i.shapesize(0.3)
+    i.width(3)
+    i.pu()
+    i.seth(90)
+    i.fd(350)
+    i.seth(-180)
+    i.pd()
+t[0].pu()
+
+delay(0)
+speed("fastest")
+ht()
+for i in colors:
+    color(i)
+    for i in range(360):
+        t[0].fd(x)
+        t[0].lt(1)
+        pu()
+        goto(t[0].pos())
+        pd()
+        t[1].fd(2*x)
+        t[1].lt(2)
+        goto(t[1].pos())
+done()
